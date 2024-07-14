b77360eb564a95d3cd46985a283ae909e7eb3beb
amine rhayem <62341652+aminerhayem123@users.noreply.github.com>
2024-07-14 01:28:44 +0100

    fixes

--- src/views/pages/packs/packs.js
+++ src/views/pages/packs/packs.js
@@ -204,7 +204,7 @@ const Packs = () => {
 
   const handleDeleteItem = async (itemId) => {
     try {
-      await axios.delete(`http://packprojectbackend-production.up.railway.app:5000/items/${itemId}`);
+      await axios.delete(`http://packprojectbackend-production.up.railway.app/items/${itemId}`);
       fetchPacks(); // Refresh the list after deletion
       window.location.reload();
     } catch (error) {
