4dea5f0bacad5390a56a5517ac4db3058dfb5e00
amine rhayem <62341652+aminerhayem123@users.noreply.github.com>
2024-07-14 02:48:40 +0100

    remove_logo

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -41,10 +41,6 @@ const AppSidebar = () => {
       }}
     >
       <CSidebarHeader className="border-bottom">
-        <CSidebarBrand to="/">
-          <CIcon customClassName="sidebar-brand-full" icon={logo} height={32} />
-          <CIcon customClassName="sidebar-brand-narrow" icon={sygnet} height={32} />
-        </CSidebarBrand>
         <CCloseButton
           className="d-lg-none"
           dark
