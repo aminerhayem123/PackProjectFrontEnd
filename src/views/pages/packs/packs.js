9e4cb3c087110745cb9287972a9d172f9d16c46f
amine rhayem <62341652+aminerhayem123@users.noreply.github.com>
2024-07-14 01:27:06 +0100

    space

--- src/views/pages/packs/packs.js
+++ src/views/pages/packs/packs.js
@@ -382,7 +382,7 @@ const handleSaleAmountChange = (e) => {
                 <PackStatusCell status={pack.status} />
                 <CTableDataCell>
                   {pack.items.map((item, idx) => (
-                    <div key={idx} className='mt-2'>
+                    <div key={idx} className='mt-5'>
                       {item.name}
                       <Button
                         variant="outline-danger"
